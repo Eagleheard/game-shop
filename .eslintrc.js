495bcc893cc9f60cfd71007bc04b3e437e5e06fc
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-01-18 21:06:04 +0300

    fix: change some settings and add .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,37 +3,60 @@ module.exports = {
     'browser': true,
     'es2021': true,
   },
+
   'extends': [
     "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended",
-        "plugin:import/errors",
         "plugin:import/warnings",
         "plugin:import/typescript",
-        "google",
         "plugin:prettier/recommended",
-        "prettier"
   ],
+
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaFeatures': {
       'jsx': true,
     },
+
     'ecmaVersion': 'latest',
     'sourceType': 'module',
   },
+
   'plugins': [
     "react",
         "@typescript-eslint",
         "prettier"
   ],
+
   'rules': {
     "require-jsdoc": "off",
     "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/ban-types": "off"
+    "@typescript-eslint/ban-types": "off",
+    "react/prop-types": "off",
+    "prefer-const": [
+      "error",
+      {
+        "destructuring": "any",
+        "ignoreReadBeforeAssign": false
+      }
+    ],
+    "prefer-destructuring": ["error", { "object": true, "array": false }],
   },
+
   "settings": {
+    "eslintConfig": {
+      "extends": ["prettier", "plugin:jsx-a11y/recommended"],
+      "settings": {
+        "import/resolver": {
+          "node": {
+            "paths": ["src"],
+            "extensions": [".js", ".jsx", ".ts", ".tsx"]
+          }
+        }
+      }
+    },
     "react": {
       "version": "detect"
     }
-  }
+  },
 };
