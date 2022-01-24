964aaaec46cdcd74afdf98c188e3b8c16c03cbda
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-01-24 16:22:59 +0300

    fix: change syntax of handleClick

--- src/screen/header/components/responsive.jsx
+++ src/screen/header/components/responsive.jsx
@@ -11,20 +11,20 @@ export const ResponsiveHeader = ({ setNavVisibility }) => {
   return (
     <div className="responsive-header">
       <nav className="mobile-nav">
-        <NavLink to="/" className="mobile-nav__item  link" onClick={() => handleClick()}>
+        <NavLink to="/" className="mobile-nav__item  link" onClick={handleClick}>
           Home
         </NavLink>
-        <NavLink to="/store" className="mobile-nav__item  link" onClick={() => handleClick()}>
+        <NavLink to="/store" className="mobile-nav__item  link" onClick={handleClick}>
           Store
         </NavLink>
-        <NavLink to="/about" className="mobile-nav__item  link" onClick={() => handleClick()}>
+        <NavLink to="/about" className="mobile-nav__item  link" onClick={handleClick}>
           About
         </NavLink>
         <div className="mobile-nav__split"></div>
-        <NavLink to="/sign-in" className="mobile-nav__item  link" onClick={() => handleClick()}>
+        <NavLink to="/sign-in" className="mobile-nav__item  link" onClick={handleClick}>
           Sign In
         </NavLink>
-        <NavLink to="/sign-up" className="mobile-nav__item  link" onClick={() => handleClick()}>
+        <NavLink to="/sign-up" className="mobile-nav__item  link" onClick={handleClick}>
           Sign Up
         </NavLink>
       </nav>
