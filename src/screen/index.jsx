83031b39f67aaf01b48acc423ce58ceac13dbff3
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-01-25 12:40:44 +0300

    fix: fix ways in src/screen/index.jsx

--- src/screen/index.jsx
+++ src/screen/index.jsx
@@ -1,5 +1,5 @@
-export { Header } from 'screen/header/components/index';
+export { Header } from 'components/header/components/index';
 export { Home } from 'screen/home/components/index';
 export { Store } from 'screen/store/components/index';
 export { About } from 'screen/about/components/index';
-export { Footer } from 'screen/footer/components/index';
+export { Footer } from 'components/footer/components/index';
