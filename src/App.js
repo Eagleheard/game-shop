6390cbdbb811f9b5e303ca8aed4ceaeb789c5aec
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-01-26 11:36:09 +0300

    fix: fix crlf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Header, About, Store, Footer } from 'screen';
+import { Header, About, Store, Footer, Home } from 'screen';
 
 import './App.css';
 
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <div className="app__content">
         <Routes>
-          <Route path="/" element={<div>home</div>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/store" element={<Store />} />
         </Routes>
--- src/screen/index.jsx
+++ src/screen/index.jsx
@@ -3,3 +3,4 @@ export { Home } from 'screen/home/components/index';
 export { Store } from 'screen/store/components/index';
 export { About } from 'screen/about/components/index';
 export { Footer } from 'components/footer/components/index';
+export { Game } from 'screen/game/components/index';
