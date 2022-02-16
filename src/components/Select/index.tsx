6481415158f060d8bc528013b7249a4732ab6552
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-02-16 23:37:23 +0300

    feat: add useOutsideClick for Select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { useOutsideClick } from 'rooks';
 
 import './styles.scss';
 
@@ -16,6 +17,12 @@ interface ISelect {
 export const Select: React.FC<ISelect> = ({ placeholder, options, style, handleSelect }) => {
   const [value, setValue] = useState<string>();
   const [isListHidden, setIsListHidden] = useState<boolean>(true);
+  const selectRef = useRef<HTMLDivElement>(null);
+  const outsideClick = () => {
+    setIsListHidden((prevValue) => !prevValue);
+  };
+
+  useOutsideClick(selectRef, outsideClick);
 
   const handleChange = (label: string) => {
     setValue(label);
@@ -40,6 +47,7 @@ export const Select: React.FC<ISelect> = ({ placeholder, options, style, handleS
         {!isListHidden &&
           options.map(({ id, label }) => (
             <div
+              ref={selectRef}
               key={id}
               className={`select__menu-item ${style}__select-menu-item`}
               onClick={() => handleChange(label)}
